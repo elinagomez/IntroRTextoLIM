07f2090ba31674cbdc963376311cb0d1e1d3f407
elinagomez <45207375+elinagomez@users.noreply.github.com>
2023-07-26 13:02:36 -0300

    clase1

---
+++ Clase1/Material/clase1_ejercicios.R
@@ -0,0 +1,22 @@
+
+#------------------------------------------------------------------------------#
+#      Introducción a la programación y análisis de texto con R                #                                    
+#                         Clase 1                                              # 
+#                      Ejercicios                                              #
+#------------------------------------------------------------------------------#
+
+
+
+## 1. Crea un objeto con el año de tu nacimiento y suma tu edad
+
+## 2. Borra el objeto
+
+## 3. Crea un vector con el apellido de los últimos 5 presidentes de tu país
+
+## 4. Crea un vector los números enteros que van del 0 al 35 y otro con los
+# números de 0 a 100 que son múltiplos de 5
+
+## 5. Averigua el tipo de los vectores y comprueba que sean los correctos y 
+# luego borra todos los objetos del ambiente
+
+
